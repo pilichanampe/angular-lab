a22f62752ef71cc4e778177c7ca33cd08bf6fac9
pilichanampe <pilichanampe@hotmail.com>
2020-06-01 00:27:47 -0300

    adding angular exercise from FaztCode

---
+++ angular-links-votes-app/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Link } from './link/link.model';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+ links: Link[];
+
+  constructor() {
+    this.links = [
+      new Link('Angular', 'http://angular.io', 100),
+      new Link('Google', 'http://google.com', 30),
+      new Link('Angular', 'http://faztweb.com', 40),
+    ];
+    console.log(this.links)
+  }
+
+  addLink(title: HTMLInputElement, link: HTMLInputElement) {
+    this.links.push(new Link(title.value, link.value));
+    title.value = '';
+    link.value = '';
+    
+    return false;
+  }
+}
